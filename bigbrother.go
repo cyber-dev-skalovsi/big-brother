bf348ddf9423741ce95c7b02e050e32396c69edc
cyber-dev-skalovsi <172505522+cyber-dev-skalovsi@users.noreply.github.com>
2026-07-28 10:00:00 +0200

    Bump version to v1.0.0 and add integration tests

--- bigbrother.go
+++ bigbrother.go
@@ -6,7 +6,7 @@ import "github.com/blang/semver"
 
 const (
 	// Version is the current version of the application.
-	Version = "0.4.0"
+	Version = "1.0.0"
 	// AppName is the name of the application.
 	AppName = "bigbrother"
 )
